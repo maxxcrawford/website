e0594ede29123d610a3569ce66040fe612424c66
Maxx Crawford <mcrawford@broco.com>
2017-09-03 23:19:32 -0500

    Added mobile navigation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,29 @@
 (function () {
     // all your code here
-  var navLoad = function () {
+  var navLoad = function (target) {
+		console.log( target );
+
+		var navItems = $('header nav a');
+    var navToggle = $('header nav button');
+
+		navItems.removeClass('active');
+		target.addClass('active')
+
     $('body').addClass('loading');
     setTimeout(function () {
       $('body').removeClass('loading');
     }, 2000);
   };
 
-  $(document).on('click', 'header nav a', navLoad);
+  $(document).on('click', 'header nav a', function(){
+		navLoad( $(this) );
+	});
+
+  $(document).on('click', 'header nav button', function(){
+		$(this).toggleClass('active');
+	});
+
+
 
     // ...
 })();
