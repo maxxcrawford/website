b82d20b8b122889b6d341f9994e83a9bbe200bf2
Maxx Crawford <maxx.crawford@gmail.com>
2018-06-06 00:09:06 -0500

    Created additional pages

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,50 +1,50 @@
 (function () {
     // all your code here
-  var navLoad = function (target) {
-		// console.log( target );
-    //
-    // console.log( target.attr('href') );
-
-		var navItems = $('header nav a');
-    var navToggle = $('header nav button');
-    var navTarget = target.attr('href');
-
-		navItems.removeClass('active');
-		target.addClass('active');
-
-    // $('body').addClass('loading');
-
-    setTimeout(function () {
-      $('main section').removeClass('active');
-      $(navTarget).addClass('active');
-    }, 1000);
-
-    setTimeout(function () {
-      navToggle.trigger('click');
-    }, 500);
-
-
-
-    // setTimeout(function () {
-    //   $('body').removeClass('loading');
-    //
-    // }, 2000);
-
-    // console.log( $(navTarget) );
-
-
-
-
-  };
-
-  $(document).on('click', 'header nav a', function(e){
-    e.preventDefault();
-		navLoad( $(this) );
-	});
-
-  $(document).on('click', 'header nav button', function(){
-		$(this).toggleClass('active');
-	});
+  // var navLoad = function (target) {
+	// 	// console.log( target );
+  //   //
+  //   // console.log( target.attr('href') );
+  //
+	// 	var navItems = $('header nav a');
+  //   var navToggle = $('header nav button');
+  //   var navTarget = target.attr('href');
+  //
+	// 	navItems.removeClass('active');
+	// 	target.addClass('active');
+  //
+  //   // $('body').addClass('loading');
+  //
+  //   setTimeout(function () {
+  //     $('main section').removeClass('active');
+  //     $(navTarget).addClass('active');
+  //   }, 1000);
+  //
+  //   setTimeout(function () {
+  //     navToggle.trigger('click');
+  //   }, 500);
+  //
+  //
+  //
+  //   // setTimeout(function () {
+  //   //   $('body').removeClass('loading');
+  //   //
+  //   // }, 2000);
+  //
+  //   // console.log( $(navTarget) );
+  //
+  //
+  //
+  //
+  // };
+  //
+  // $(document).on('click', 'header nav a', function(e){
+  //   e.preventDefault();
+	// 	navLoad( $(this) );
+	// });
+  //
+  // $(document).on('click', 'header nav button', function(){
+	// 	$(this).toggleClass('active');
+	// });
 
 $(window).on('load', function(){
   var $headerTitle = $('header h1');
